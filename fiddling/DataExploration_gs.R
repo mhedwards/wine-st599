c343973b2a1185e5b7a49fc38b3225ae1262a878
Gina S <gina.shellhammer@gmail.com>
2014-05-22 22:57:03 -0700

    cause I want to sync
    some added stuff about supervised pcr - didn’t work

--- fiddling/DataExploration_gs.R
+++ fiddling/DataExploration_gs.R
@@ -42,3 +42,96 @@ summary(pcr.red.5)
 ##lowest AIC/BIC looks like pcr with 3 pc's ## This may be a start.
 AIC(pcr.red.1, pcr.red.2, pcr.red.3, pcr.red.4, pcr.red.5)
 BIC(pcr.red.1, pcr.red.2, pcr.red.3, pcr.red.4, pcr.red.5)
+
+###Reading about supervised pcr as a better prediction tool
+#http://statweb.stanford.edu/~tibs/superpc/tutorial.html
+
+featurenames <- paste("feature",as.character(1:997),sep="")
+red.y<-as.numeric(red.y)
+str(red.preds)
+str(red.y)
+data<-list(x=(red.preds,y=as.numeric(red.y), featurenames = featurenames)
+data.test<-list(x=red.test[,1:11],y=as.numeric(red.test[, 12]))
+
+# train  the model. This step just computes the  scores for each feature
+
+train.obj<- superpc.train(data, type="regression")
+
+# note for regression (non-survival) data, we leave the component "censoring.status"
+# out of the data object, and call superpc.train with type="regression".
+# otherwise the superpc commands are all the same
+
+
+
+# cross-validate the model
+
+cv.obj<-superpc.cv(train.obj, data)
+
+#plot the cross-validation curves. From this plot we see that the 1st 
+# principal component is significant and the best threshold  is around 0.7
+
+superpc.plotcv(cv.obj)
+
+
+#See pdf version of the cv plot 
+
+# here we have the luxury of  test data, so we can compute the  likelihood ratio statistic
+# over the test data and plot them. We see that the threshold of 0.7
+# works pretty well
+#  
+
+lrtest.obj<-superpc.lrtest.curv(train.obj, data,data.test)
+
+superpc.plot.lrtest(lrtest.obj)
+
+#See pdf version of the lrtest plot
+
+
+# now we derive the predictor of survival  for the test data, 
+# and then then use it
+# as the predictor in a Cox model . We see that the 1st supervised PC is
+# highly significant; the next two are not
+
+fit.cts<- superpc.predict(train.obj, data, data.test, threshold=0.7, n.components=3, prediction.type="continuous")
+
+
+superpc.fit.to.outcome(train.obj, data.test, fit.cts$v.pred)
+
+
+# sometimes a discrete (categorical) predictor is attractive.
+# Here we form two groups by cutting the predictor at its median
+#and then plot Kaplan-Meier curves for the two groups
+
+fit.groups<- superpc.predict(train.obj, data, data.test, threshold=0.7, n.components=1, prediction.type="discrete")
+
+superpc.fit.to.outcome(train.obj, data.test, fit.groups$v.pred)
+
+plot(survfit(Surv(data.test$y,data.test$censoring.status)~fit.groups$v.pred), col=2:3, xlab="time", ylab="Prob survival")
+
+#See pdf version of the survival plot
+
+
+# Finally, we look for a predictor of survival a small number of
+#genes (rather than all 1000 genes). We do this by computing an importance
+# score for each equal its correlation with the supervised PC predictor.
+# Then we soft threshold the importance scores, and use the shrunken
+# scores as gene weights to from a reduced predictor. Cross-validation
+# gives us an estimate of the best amount to shrink and an idea of
+#how well the shrunken predictor works.
+
+
+fit.red<- superpc.predict.red(train.obj, data, data.test, threshold=0.7)
+
+fit.redcv<- superpc.predict.red.cv(fit.red, cv.obj,  data,  threshold=0.7)
+
+superpc.plotred.lrtest(fit.redcv)
+
+#See pdf version of this plot
+
+
+# Finally we list the significant genes, in order of decreasing importance score
+
+superpc.listfeatures(data.test, train.obj, fit.red, 1, shrinkage=0.17)
+
+
+
