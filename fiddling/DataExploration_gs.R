91aa1c6f96812defc356c5d2aaa92e61c4eeaef8
Gina S <gina.shellhammer@gmail.com>
2014-05-27 00:22:12 -0700

    MLR and ordinal regression
    mlr with predictions
    add data mining paper from this data set.

--- fiddling/DataExploration_gs.R
+++ fiddling/DataExploration_gs.R
@@ -89,98 +89,31 @@ summary(pcr.white.6)
 AIC(pcr.white.1, pcr.white.2, pcr.white.3, pcr.white.4, pcr.white.5, pcr.white.6)
 BIC(pcr.white.1, pcr.white.2, pcr.white.3, pcr.white.4, pcr.white.5, pcr.white.6)
 ## looks like regression with 5 pcs has lowest BIC.
-#Now look at modeling with best subset
+## ordianl regerssion using raw data
+or.white.5<-polr(as.factor(whitewine$quality)~fixed.acidity + volatile.acidity + citric.acid + 
+                   residual.sugar + chlorides + total.sulfur.dioxide + density +   pH +
+                   sulphates + alcohol + free.sulfur.dioxide, data = white)
+if(require(MASS)) { ## dropterm, stepAIC, 
+ or1 <- polr(as.factor(whitewine$quality)~fixed.acidity + volatile.acidity + citric.acid + 
+               residual.sugar + chlorides + total.sulfur.dioxide + density +   pH +
+               sulphates + alcohol + free.sulfur.dioxide, data = whitewine)
+  dropterm(m1, test = "Chi")
+  or3<- stepAIC(or1)
+  summary(or3)
+}
+###on scaled data 
+white.y<-white$quality
+head(white)
 
+white.x<-scale(white[,1:11])
 
-###Reading about supervised pcr as a better prediction tool
-#http://statweb.stanford.edu/~tibs/superpc/tutorial.html
-
-featurenames <- paste("feature",as.character(1:997),sep="")
-red.y<-as.numeric(red.y)
-str(red.preds)
-str(red.y)
-data<-list(x=(red.preds,y=as.numeric(red.y), featurenames = featurenames)
-data.test<-list(x=red.test[,1:11],y=as.numeric(red.test[, 12]))
-
-# train  the model. This step just computes the  scores for each feature
-superpc.train
-train.obj<- superpc.train(data, type="regression")
-
-# note for regression (non-survival) data, we leave the component "censoring.status"
-# out of the data object, and call superpc.train with type="regression".
-# otherwise the superpc commands are all the same
-
-
-
-# cross-validate the model
-
-cv.obj<-superpc.cv(train.obj, data)
-
-#plot the cross-validation curves. From this plot we see that the 1st 
-# principal component is significant and the best threshold  is around 0.7
-
-superpc.plotcv(cv.obj)
-
-
-#See pdf version of the cv plot 
-
-# here we have the luxury of  test data, so we can compute the  likelihood ratio statistic
-# over the test data and plot them. We see that the threshold of 0.7
-# works pretty well
-#  
-
-lrtest.obj<-superpc.lrtest.curv(train.obj, data,data.test)
-
-superpc.plot.lrtest(lrtest.obj)
-
-#See pdf version of the lrtest plot
-
-
-# now we derive the predictor of survival  for the test data, 
-# and then then use it
-# as the predictor in a Cox model . We see that the 1st supervised PC is
-# highly significant; the next two are not
-
-fit.cts<- superpc.predict(train.obj, data, data.test, threshold=0.7, n.components=3, prediction.type="continuous")
-
-
-superpc.fit.to.outcome(train.obj, data.test, fit.cts$v.pred)
-
-
-# sometimes a discrete (categorical) predictor is attractive.
-# Here we form two groups by cutting the predictor at its median
-#and then plot Kaplan-Meier curves for the two groups
-
-fit.groups<- superpc.predict(train.obj, data, data.test, threshold=0.7, n.components=1, prediction.type="discrete")
-
-superpc.fit.to.outcome(train.obj, data.test, fit.groups$v.pred)
-
-plot(survfit(Surv(data.test$y,data.test$censoring.status)~fit.groups$v.pred), col=2:3, xlab="time", ylab="Prob survival")
-
-#See pdf version of the survival plot
-
-
-# Finally, we look for a predictor of survival a small number of
-#genes (rather than all 1000 genes). We do this by computing an importance
-# score for each equal its correlation with the supervised PC predictor.
-# Then we soft threshold the importance scores, and use the shrunken
-# scores as gene weights to from a reduced predictor. Cross-validation
-# gives us an estimate of the best amount to shrink and an idea of
-#how well the shrunken predictor works.
-
-
-fit.red<- superpc.predict.red(train.obj, data, data.test, threshold=0.7)
-
-fit.redcv<- superpc.predict.red.cv(fit.red, cv.obj,  data,  threshold=0.7)
-
-superpc.plotred.lrtest(fit.redcv)
-
-#See pdf version of this plot
-
-
-# Finally we list the significant genes, in order of decreasing importance score
-
-superpc.listfeatures(data.test, train.obj, fit.red, 1, shrinkage=0.17)
-
-
-
+wwscale<-as.data.frame(cbind(white.y, white.x))
+head(wwscale)
+if(require(MASS)) { ## dropterm, stepAIC, 
+  or1 <- polr(as.factor(white.y)~fixed.acidity + volatile.acidity + citric.acid + 
+                residual.sugar + chlorides + total.sulfur.dioxide + density +   pH +
+                sulphates + alcohol + free.sulfur.dioxide, data = wwscale)
+  dropterm(m1, test = "Chi")
+  or3<- stepAIC(or1)
+  summary(or3)
+}
--- fiddling/modeling_gs.R
+++ fiddling/modeling_gs.R
@@ -5,20 +5,48 @@ white<-read.csv("/Users/Babydoll/Documents/BigData/wine-st599/data/whitewine-tra
 install.packages("ordinal")
 library("ordinal")
 ## need to scale the predictors
-white.y<-whit$quality
-head(white)
-white.preds<-scale(white[,1:11])
+#white.y<-white$quality
+#head(white)
 
-wwscale<-as.data.frame(cbind(white.y, white.preds))
-head(wwscale)
+#white.x<-scale(white[,1:11])
+
+#wwscale<-as.data.frame(cbind(white.y, white.x))
+#head(wwscale)
 if(require(MASS)) { ## dropterm, stepAIC, 
-  m1 <- clm(as.factor(white.y)~fixed.acidity + volatile.acidity + citric.acid + 
+  m1 <- lm(white$quality~fixed.acidity + volatile.acidity + citric.acid + 
               residual.sugar + chlorides + total.sulfur.dioxide + density +   pH +
-              sulphates + alcohol + free.sulfur.dioxide, data = wwscale)
+              sulphates + alcohol + free.sulfur.dioxide, data = white)
   dropterm(m1, test = "Chi")
   m3 <- stepAIC(m1)
   summary(m3)
 }
-head(m3$model)
-head(ww)
-##built model now need to test on rest of data frame
+m3$coef
+
+means<-apply(white, 2, mean)
+sds<-apply(white, 2, sd)
+
+
+fa<-1.045317e-01
+va<--1.950710e+00
+resug<-9.420111e-02 
+dens<--1.952274e+02
+ph<-8.837933e-01       
+sul<-6.237002e-01       
+alc<-1.307144e-01
+fsd<-2.248178e-03
+m3$coef
+white.test<-read.csv("/Users/Babydoll/Documents/BigData/wine-st599/data/whitewine-testset.csv", header = TRUE)
+white.test$Int<-1.945886e+02
+
+
+predicted = (white.test$Int +(white.test$fixed.acidity*fa)+ (white.test$volatile.acidity*va)+
+  (white.test$residual.sugar*resug) + (white.test$density*dens) + (white.test$pH*ph) + 
+  (white.test$sulphates*sul) + (white.test$alcoho*alc)+ (white.test$free.sulfur.dioxide*fsd))
+##OK now to test
+## just using a blunt instrument here. I may want to look at 6-6.75 as 6 and above 6.75 as 7.
+white.test$predict<-round(predicted)
+
+sum(white.test$predict == white.test$quality)
+table(white.test$predict == white.test$quality)
+with(white.test, table(white.test$predict == white.test$quali
+
